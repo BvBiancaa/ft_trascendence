3e8766b14071842dfb718a293fab53169091ec12
Bianca <111921780+BvBiancaa@users.noreply.github.com>
2023-07-31 09:25:41 +0200

    Delete src directory

--- src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export const getDate = (input: string) => {
-  const date = new Date(input);
-  const month = date.toLocaleDateString("default", { month: "short" });
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minutes = date.getMinutes();
-
-  return `${month} ${day} at ${hour}:${minutes}`;
-};
